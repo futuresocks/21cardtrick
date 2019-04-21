c2d82ec341081ff15a4681262ba43d3825a1422f
Colin Bell <colin@dogoodercomics.co.uk>
2019-04-21 14:44:54 +0100

    deals cards into three columns

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template lang="html">
+  <div id="cardDisplay">
   <h1>IMMA READ YOUR MIND</h1>
+  <ul>
+    <li v-for="card in columnA"><img :src="card.image"/></li>
+  </ul>
+  <ul>
+    <li v-for="card in columnB"><img :src="card.image"/></li>
+  </ul>
+  <ul>
+    <li v-for="card in columnC"><img :src="card.image"/></li>
+  </ul>
+  <button v-on:click="deal">DEAL</button>
+</div>
 </template>
 
 <script>
 export default {
   data(){
     return {
-      cards: []
+      cards: [],
+      columnA: [],
+      columnB: [],
+      columnC: [],
     }
   },
   mounted(){
@@ -17,9 +32,27 @@ export default {
       .then(res => res.json())
       .then(json => this.cards = json.cards)
     })
+  },
+  methods: {
+    deal(){
+      debugger;
+      while(this.cards.length){
+        const columns = [this.columnA, this.columnB, this.columnC];
+        columns.forEach((column) => {
+          column.push(this.cards.shift());
+        })
+      }
+    }
   }
 }
 </script>
 
 <style lang="css" scoped>
+img {
+  height: 100px;
+}
+
+#cardDisplay {
+  display: flex;
+}
 </style>
